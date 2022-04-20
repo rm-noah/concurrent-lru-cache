bb926bdc927b08234e077ed21cf81f85bb4bb285
Noah <noahquinn123@hotmail.co.uk>
2022-04-20 21:11:07 +0100

    Removed ReentrantReadWriteLock and replace it with synchronised keyword

--- src/main/java/com/noah/ConcurrentLRUCache.java
+++ src/main/java/com/noah/ConcurrentLRUCache.java
@@ -1,7 +1,6 @@
 package com.noah;
 
 import java.util.*;
-import java.util.concurrent.locks.*;
 
 /**
  * Concurrent LRU cache stores a hash set of the given type
@@ -25,9 +24,10 @@ public class ConcurrentLRUCache<T> {
      */
     private final HashMap<T, Integer> hits;
 
+    /**
+     * Maximum size of the LinkedHashSet
+     */
     private final int capacity;
-
-    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     public ConcurrentLRUCache(int capacity) {
         this.capacity = capacity;
@@ -40,13 +40,8 @@ public class ConcurrentLRUCache<T> {
      *
      * @return Size of cache
      */
-    public int size() {
-        lock.readLock().lock();
-        try {
-            return set.size();
-        } finally {
-            lock.readLock().unlock();
-        }
+    public synchronized int size() {
+        return set.size();
     }
 
     /**
@@ -55,30 +50,18 @@ public class ConcurrentLRUCache<T> {
      * @param key Desired element
      * @return Optional object containing a key if the key exists in the cache, else an empty Optional
      */
-    public Optional<T> find(T key) {
-        // For concurrent projects we'll lock the read
-        // Alternatively we could use the synchronised keyword which would
-        // work fine but manually locking/unlocking gives us more control.
-        lock.readLock().lock();
-        lock.writeLock().lock();
+    public synchronized Optional<T> find(T key) {
+        // Opted for Optionals as the spec doesn't specify a default value to return.
+        // "null" could work, but it depends on what the project uses.
+        Optional<T> record = Optional.empty();
 
-        try {
-            // Opted for Optionals as the spec doesn't specify a default value to return.
-            // "null" could work, but it depends on what the project uses.
-            Optional<T> record = Optional.empty();
-
-            if (set.contains(key)) {
-                if (hits.containsKey(key)) {
-                    hits.put(key, hits.get(key) + 1);
-                }
-                record = Optional.of(key);
+        if (set.contains(key)) {
+            if (hits.containsKey(key)) {
+                hits.put(key, hits.get(key) + 1);
             }
-            return record;
-        } finally {
-            // Must always unlock in the `finally` block to prevent deadlock
-            lock.readLock().unlock();
-            lock.writeLock().unlock();
+            record = Optional.of(key);
         }
+        return record;
     }
 
     /**
@@ -86,29 +69,24 @@ public class ConcurrentLRUCache<T> {
      *
      * @param association Element to add to the cache
      */
-    public void add(T association) {
-        // Locking the write operation to prevent other threads overwriting
-        lock.writeLock().lock();
-        try {
-            if (set.size() == capacity) {
-                // Remove least used key
-                Optional<T> stale = findLeastFrequent();
-                if (stale.isPresent()) {
-                    set.remove(stale.get());
-                    hits.remove(stale.get());
-                }
+    public synchronized void add(T association) {
+        System.out.println("Adding " + association);
+        if (set.size() == capacity) {
+            // Remove least used key
+            Optional<T> stale = findLeastFrequent();
+            if (stale.isPresent()) {
+                set.remove(stale.get());
+                hits.remove(stale.get());
             }
-            set.add(association);
-            hits.put(association, 1);
-        } finally {
-            lock.writeLock().unlock();
         }
+        set.add(association);
+        hits.put(association, 1);
     }
 
     /**
      * Returns the popularity of a given element, 0 if the element does not exist
      */
-    public int getPopularity(T key) {
+    public synchronized int getPopularity(T key) {
         return hits.getOrDefault(key, 0);
     }
 
@@ -119,15 +97,14 @@ public class ConcurrentLRUCache<T> {
      * @return Integer to remove, else an empty Optional
      */
     private Optional<T> findLeastFrequent() {
-        lock.readLock().lock();
-        try {
-            Optional<T> min = Optional.empty();
-            if (hits.isEmpty()) {
-                return min;
-            }
-            return set.stream().min(Comparator.comparing(hits::get));
-        } finally {
-            lock.readLock().unlock();
+        Optional<T> min = Optional.empty();
+        if (hits.isEmpty()) {
+            return min;
         }
+        return set.stream().min(Comparator.comparing(hits::get));
+    }
+
+    public void contents() {
+        set.forEach(System.out::println);
     }
 }
